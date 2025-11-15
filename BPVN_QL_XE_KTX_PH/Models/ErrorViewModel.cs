76fa94c0c8c41527a18460b5716af213c714a76e
THIEUXK1 <163542844+THIEUXK1@users.noreply.github.com>
2025-11-15 09:03:33 +0700

    Add project files.

---
+++ BPVN_QL_XE_KTX_PH/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BPVN_QL_XE_KTX_PH.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
